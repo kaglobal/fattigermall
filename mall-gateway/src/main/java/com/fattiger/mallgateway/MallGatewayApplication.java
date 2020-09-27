dbb44652766005dcea172a2cd7c8f16394a148f8
fuhang <591051702@qq.com>
2020-09-27 18:46:26 +0800

    主项目推送

---
+++ mall-gateway/src/main/java/com/fattiger/mallgateway/MallGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.fattiger.mallgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import reactor.core.publisher.Mono;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallGatewayApplication.class, args);
+    }
+
+}
