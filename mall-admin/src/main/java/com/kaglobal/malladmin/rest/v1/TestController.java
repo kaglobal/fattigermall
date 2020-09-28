7e3bdea0b9188b565a22954a6dfee7727a876da2
fuhang <591051702@qq.com>
2020-09-28 18:47:51 +0800

    1.多模块 整合springcloud gateway 2.gateway Knife4j 统一多微服务模块api文档

--- mall-admin/src/main/java/com/kaglobal/malladmin/rest/v1/TestController.java
+++ mall-admin/src/main/java/com/kaglobal/malladmin/rest/v1/TestController.java
@@ -1,5 +1,8 @@
 package com.kaglobal.malladmin.rest.v1;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Version 1.0.0
  **/
 @RestController
+@Api(tags = "测试记录",description = "测试记录")
 public class TestController {
 
     @RequestMapping(value = "/test",method = RequestMethod.GET)
@@ -25,4 +29,10 @@ public class TestController {
         return "hello1";
     }
 
+    @ApiOperation(value = "1.0.0 新增输血记录数据", httpMethod = "POST", notes = "1.0.0 新增输血记录信息", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping("/v1//blood-transfusion-record")
+    public String  AnesthAfterInsert(){
+
+        return  "ss";
+    }
 }
--- mall-gateway/src/main/java/com/fattiger/mallgateway/MallGatewayApplication.java
+++ mall-gateway/src/main/java/com/fattiger/mallgateway/MallGatewayApplication.java
@@ -3,10 +3,8 @@ package com.fattiger.mallgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import reactor.core.publisher.Mono;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class MallGatewayApplication {
 
