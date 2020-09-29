feb446848fe5f4864de74e871c8e17371c89c6b6
fuhang <591051702@qq.com>
2020-09-29 11:00:11 +0800

    订单模块

---
+++ mall-order/src/main/java/com/fattiger/mallorder/MallOrderApplication.java
@@ -0,0 +1,15 @@
+package com.fattiger.mallorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MallOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallOrderApplication.class, args);
+    }
+
+}
