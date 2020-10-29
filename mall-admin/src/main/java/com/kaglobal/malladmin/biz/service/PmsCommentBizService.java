d5f3ce05ca0db85721298a01ed0fc2601ef4070b
fuhang <591051702@qq.com>
2020-10-29 19:30:51 +0800

    Dao代码

---
+++ mall-admin/src/main/java/com/kaglobal/malladmin/biz/service/PmsCommentBizService.java
@@ -0,0 +1,12 @@
+package com.kaglobal.malladmin.biz.service;
+
+
+/**
+ * 商品评价表 业务Service
+ * 
+ * @author bianj
+ * @version 2020-10-19
+ */
+public interface PmsCommentBizService{
+
+}
---
+++ mall-admin/src/main/java/com/kaglobal/malladmin/biz/service/PmsCommentReplayBizService.java
@@ -0,0 +1,12 @@
+package com.kaglobal.malladmin.biz.service;
+
+
+/**
+ * 产品评价回复表 业务Service
+ * 
+ * @author bianj
+ * @version 2020-10-19
+ */
+public interface PmsCommentReplayBizService{
+
+}
