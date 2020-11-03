b84196f9816abfc34dcf6fb16c917102a77f625e
fuhang <591051702@qq.com>
2020-11-03 17:42:04 +0800

    业务代码

---
+++ mall-admin/src/main/java/com/kaglobal/malladmin/biz/service/PmsProductAttributeBizService.java
@@ -0,0 +1,12 @@
+package com.kaglobal.malladmin.biz.service;
+
+
+/**
+ * 商品属性参数表 业务Service
+ * 
+ * @author bianj
+ * @version 2020-10-19
+ */
+public interface PmsProductAttributeBizService{
+
+}
---
+++ mall-admin/src/main/java/com/kaglobal/malladmin/biz/service/PmsProductAttributeValueBizService.java
@@ -0,0 +1,12 @@
+package com.kaglobal.malladmin.biz.service;
+
+
+/**
+ * 存储产品参数信息的表 业务Service
+ * 
+ * @author bianj
+ * @version 2020-10-19
+ */
+public interface PmsProductAttributeValueBizService{
+
+}
